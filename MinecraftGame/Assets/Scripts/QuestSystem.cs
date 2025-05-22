8a91b7f93092f330ea494f18ce246281440269c0
kryakikrya <gon4.egor@gmail.com>
2025-05-23 00:22:20 +0300

    sigma check

---
+++ MinecraftGame/Assets/Scripts/QuestSystem.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestSystem : MonoBehaviour
+{
+    [SerializeField] private List<Quest> _quests = new List<Quest>();
+
+}
